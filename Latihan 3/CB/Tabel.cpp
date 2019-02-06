b78f28d79124e8089020efc43c41077d3361bae0
rizkirak <44920674+rizkirak@users.noreply.github.com>
2019-02-07 01:44:12 +0700

    Add files via upload

---
+++ Latihan 3/CB/Tabel.cpp
@@ -0,0 +1,115 @@
+#include "Tabel.h"
+#include <iostream>
+
+using namespace std;
+
+Tabel::Tabel()
+{
+    size=defaultSize;
+    data_=new int[size];
+}
+
+Tabel::Tabel(int size)
+{
+    this->size=size;
+    data_=new int[this->size];
+}
+
+Tabel::Tabel(const Tabel& t)
+{
+    int i;
+
+    size=t.size;
+    data_=new int[size];
+    for (i=0;i<=size;i++)
+    {
+        data_[i]=t.data_[i];
+    }
+
+
+
+
+    //return n;
+}
+
+Tabel::~Tabel()
+{
+    //delete [] data_;
+
+
+
+    //return b;
+}
+
+Tabel& Tabel::operator=(const Tabel& rhs)
+{
+    int i;
+    if (this!=&rhs)
+    {
+        size=rhs.size;
+        for (i=0;i<=size;i++)
+        {
+            data_[i]=rhs.data_[i];
+        }
+    }
+    return *this;
+
+}
+
+void Tabel::UData(int a)
+{
+    int i;
+
+    for (i=0;i<=size;i++)
+    {
+        data_[i]=a;
+    }
+
+}
+
+
+
+
+
+
+void Tabel::keluaran()
+{
+    int i;
+    cout<<size<<endl;
+    cout<<"Tabel :"<<endl;
+    for (i=0;i<=size;i++)
+    {
+        cout<<"Data ke -"<<i<<":"<<data_[i]<<endl;
+
+
+
+
+    }
+
+
+
+
+    //return a;
+}
+
+/*
+void Pecahan::keluaran()
+{
+    cout<<n<<endl;
+    cout<<a<<endl;
+    cout<<b<<endl;
+}
+*/
+
+
+
+
+
+
+/*
+Pecahan::~Pecahan()
+{
+    //dtor
+}
+*/
+
