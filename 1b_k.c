04d050ad216472a946af83412e9c735ec7389c26
rizkirak <44920674+rizkirak@users.noreply.github.com>
2019-08-22 02:53:07 +0700

    Add files via upload

---
+++ 1b_k.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*KAMUS*/
+    float I, R, V;
+
+
+
+
+
+
+    /*ALGORITMA*/
+    scanf("%f", &I);
+    scanf("%f", &R);
+    printf("%f", I*R);
+
+
+
+
+
+
+
+
+    return 0;
+}
