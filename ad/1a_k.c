c48d3003bfa0b5b70a3975627be0649aac8611d3
rizkirak <44920674+rizkirak@users.noreply.github.com>
2019-08-22 02:52:45 +0700

    Add files via upload

---
+++ ad/1a_k.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //KAMUS
+    char nama[100];
+
+
+
+    /*ALGORITMA*/
+    scanf("%s", nama);
+    printf("Hello, %s", nama);
+
+
+
+
+
+
+
+
+    return 0;
+}
